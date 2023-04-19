32236b1722d7ed04ea325f33751f1ee28cfb5034
Paulo Gomes <pjgomes85@hotmail.com>
2023-04-19 17:37:33 +0100

    moving env file

--- server.js
+++ server.js
@@ -32,3 +32,18 @@ client.on('message', (channel, tags, message, self) => {
 
   const [raw, command, argument] = message.match(regexpCommand);
 
+  const {response} = commands[command] || {};
+
+  if (typeof response === 'function' ) {
+    client.say(channel, response(tags.username))
+  } else if (typeof response === 'string') {
+    client.say(channel, response)
+  }
+
+
+  // {
+  //   client.say(channel, `Message "${message}" was sent by ${tags.username}`)
+  // }
+
+	console.log(`${tags['display-name']}: ${message}`);
+});
